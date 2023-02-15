292718fb17d09e22c706b5d889c1bb402ba24103
Aleksandar <exunys@gang.email>
2023-02-15 15:58:37 +0100

    Update Aimbot.lua

--- Aimbot.lua
+++ Aimbot.lua
@@ -1,7 +1,6 @@
 --[[
 
 	Aimbot Module [westbound.pro] by Exunys © CC0 1.0 Universal (2023)
-
 	https://github.com/Exunys
 
 ]]
@@ -12,7 +11,7 @@ local pcall, getgenv, next, setmetatable, Vector2new, CFramenew, Color3fromRGB, 
 
 --// Launching checks
 
-if not getgenv().AirTeam_westboundpro or getgenv().AirTeam_westboundpro.Aimbot then return end
+if not getgenv().AirTeam_westboundpro or not getgenv().AirTeam_westboundpro.Aimbot then return end
 
 --// Services
 
@@ -29,30 +28,7 @@ local RequiredDistance, Typing, Running, ServiceConnections, Parts, Animation, O
 
 --// Environment
 
-getgenv().AirTeam_westboundpro.Aimbot = {
-	Settings = {
-		Enabled = false,
-		WallCheck = false,
-		Sensitivity = 0, -- Animation length (in seconds) before fully locking onto target
-		ThirdPerson = false, -- Uses mousemoverel instead of CFrame to support locking in third person (could be choppy)
-		ThirdPersonSensitivity = 3,
-		TriggerKey = "MouseButton2",
-		Toggle = false,
-	},
-
-	FOVSettings = {
-		Color = Color3fromRGB(255, 255, 255),
-		LockedColor = Color3fromRGB(255, 70, 70),
-		Transparency = 0.5,
-		Sides = 60,
-		Thickness = 1,
-		Filled = false
-	},
-
-	FOVCircle = Drawing.new("Circle")
-}
-
-local ParentEnvironment, Environment = getgenv().AirTeam_westboundpro.Settings, getgenv().AirTeam_westboundpro.Aimbot
+local FOVCircle = Drawing.new("Circle")
 
 --// Core Functions
 
@@ -61,34 +37,34 @@ local function ConvertVector(Vector)
 end
 
 local function CancelLock()
-	Environment.Locked = nil
+	getgenv().AirTeam_westboundpro.Aimbot.Locked = nil
 	if Animation then Animation:Cancel() end
-	Environment.FOVCircle.Color = Environment.FOVSettings.Color
+	FOVCircle.Color = getgenv().AirTeam_westboundpro.Aimbot.FOVSettings.Color
 	UserInputService.MouseDeltaSensitivity = OriginalSensitivity
 end
 
 local function GetClosestPlayer()
-	local HitPart = ParentEnvironment.HitPart == "Random" and Parts[math.random(1, #Parts - 1)] or ParentEnvironment.HitPart
+	local HitPart = getgenv().AirTeam_westboundpro.Settings.HitPart == "Random" and Parts[math.random(1, #Parts - 1)] or getgenv().AirTeam_westboundpro.Settings.HitPart
 
-	if not Environment.Locked then
-		RequiredDistance = (ParentEnvironment.FOV.Enabled and ParentEnvironment.FOV.Amount or 2000)
+	if not getgenv().AirTeam_westboundpro.Aimbot.Locked then
+		RequiredDistance = (getgenv().AirTeam_westboundpro.Settings.FOV.Enabled and getgenv().AirTeam_westboundpro.Settings.FOV.Amount or 2000)
 
 		for _, v in next, Players:GetPlayers() do
-			if v ~= LocalPlayer and v.Character and v.Character:FindFirstChild(ParentEnvironment.HitPart) and v.Character:FindFirstChildOfClass("Humanoid") then
+			if v ~= LocalPlayer and v.Character and v.Character:FindFirstChild(getgenv().AirTeam_westboundpro.Settings.HitPart) and v.Character:FindFirstChildOfClass("Humanoid") then
 				if LocalPlayer.Team == game.Teams.Cowboys and v.TeamColor == LocalPlayer.TeamColor then continue end
 				if v.Character:FindFirstChildOfClass("Humanoid").Health <= 0 then continue end
-				if Environment.Settings.WallCheck and #(Camera:GetPartsObscuringTarget({v.Character[HitPart].Position}, v.Character:GetDescendants())) > 0 then continue end
+				if getgenv().AirTeam_westboundpro.Aimbot.Settings.WallCheck and #(Camera:GetPartsObscuringTarget({v.Character[HitPart].Position}, v.Character:GetDescendants())) > 0 then continue end
 
 				local Vector, OnScreen = Camera:WorldToViewportPoint(v.Character[HitPart].Position); Vector = ConvertVector(Vector)
 				local Distance = (UserInputService:GetMouseLocation() - Vector).Magnitude
 
 				if Distance < RequiredDistance and OnScreen then
 					RequiredDistance = Distance
-					Environment.Locked = v
+					getgenv().AirTeam_westboundpro.Aimbot.Locked = v
 				end
 			end
 		end
-	elseif (UserInputService:GetMouseLocation() - ConvertVector(Camera:WorldToViewportPoint(Environment.Locked.Character[ParentEnvironment.HitPart].Position))).Magnitude > RequiredDistance then
+	elseif (UserInputService:GetMouseLocation() - ConvertVector(Camera:WorldToViewportPoint(getgenv().AirTeam_westboundpro.Aimbot.Locked.Character[getgenv().AirTeam_westboundpro.Settings.HitPart].Position))).Magnitude > RequiredDistance then
 		CancelLock()
 	end
 end
@@ -97,39 +73,39 @@ local function Load()
 	OriginalSensitivity = UserInputService.MouseDeltaSensitivity
 
 	ServiceConnections.RenderSteppedConnection = RunService.RenderStepped:Connect(function()
-		if ParentEnvironment.FOV.Enabled then
-			Environment.FOVCircle.Radius = ParentEnvironment.FOV.Amount
-			Environment.FOVCircle.Thickness = Environment.FOVSettings.Thickness
-			Environment.FOVCircle.Filled = Environment.FOVSettings.Filled
-			Environment.FOVCircle.NumSides = Environment.FOVSettings.Sides
-			Environment.FOVCircle.Color = Environment.FOVSettings.Color
-			Environment.FOVCircle.Transparency = Environment.FOVSettings.Transparency
-			Environment.FOVCircle.Visible = ParentEnvironment.FOV.Enabled
-			Environment.FOVCircle.Position = Vector2new(UserInputService:GetMouseLocation().X, UserInputService:GetMouseLocation().Y)
+		if getgenv().AirTeam_westboundpro.Settings.FOV.Enabled then
+			FOVCircle.Radius = getgenv().AirTeam_westboundpro.Settings.FOV.Amount
+			FOVCircle.Thickness = getgenv().AirTeam_westboundpro.Aimbot.FOVSettings.Thickness
+			FOVCircle.Filled = getgenv().AirTeam_westboundpro.Aimbot.FOVSettings.Filled
+			FOVCircle.NumSides = getgenv().AirTeam_westboundpro.Aimbot.FOVSettings.Sides
+			FOVCircle.Color = getgenv().AirTeam_westboundpro.Aimbot.FOVSettings.Color
+			FOVCircle.Transparency = getgenv().AirTeam_westboundpro.Aimbot.FOVSettings.Transparency
+			FOVCircle.Visible = getgenv().AirTeam_westboundpro.Settings.FOV.Enabled
+			FOVCircle.Position = Vector2new(UserInputService:GetMouseLocation().X, UserInputService:GetMouseLocation().Y)
 		else
-			Environment.FOVCircle.Visible = false
+			FOVCircle.Visible = false
 		end
 
-		if Running and Environment.Settings.Enabled then
+		if Running and getgenv().AirTeam_westboundpro.Aimbot.Settings.Enabled then
 			GetClosestPlayer()
 
-			if Environment.Locked then
-				if Environment.Settings.ThirdPerson then
-					local Vector = Camera:WorldToViewportPoint(Environment.Locked.Character[ParentEnvironment.HitPart].Position)
+			if getgenv().AirTeam_westboundpro.Aimbot.Locked then
+				if getgenv().AirTeam_westboundpro.Aimbot.Settings.ThirdPerson then
+					local Vector = Camera:WorldToViewportPoint(getgenv().AirTeam_westboundpro.Aimbot.Locked.Character[getgenv().AirTeam_westboundpro.Settings.HitPart].Position)
 
-					mousemoverel((Vector.X - UserInputService:GetMouseLocation().X) * Environment.Settings.ThirdPersonSensitivity, (Vector.Y - UserInputService:GetMouseLocation().Y) * Environment.Settings.ThirdPersonSensitivity)
+					mousemoverel((Vector.X - UserInputService:GetMouseLocation().X) * getgenv().AirTeam_westboundpro.Aimbot.Settings.ThirdPersonSensitivity, (Vector.Y - UserInputService:GetMouseLocation().Y) * getgenv().AirTeam_westboundpro.Aimbot.Settings.ThirdPersonSensitivity)
 				else
-					if Environment.Settings.Sensitivity > 0 then
-						Animation = TweenService:Create(Camera, TweenInfo.new(Environment.Settings.Sensitivity, Enum.EasingStyle.Sine, Enum.EasingDirection.Out), {CFrame = CFramenew(Camera.CFrame.Position, Environment.Locked.Character[Environment.Settings.LockPart].Position)})
+					if getgenv().AirTeam_westboundpro.Aimbot.Settings.Sensitivity > 0 then
+						Animation = TweenService:Create(Camera, TweenInfo.new(getgenv().AirTeam_westboundpro.Aimbot.Settings.Sensitivity, Enum.EasingStyle.Sine, Enum.EasingDirection.Out), {CFrame = CFramenew(Camera.CFrame.Position, getgenv().AirTeam_westboundpro.Aimbot.Locked.Character[getgenv().AirTeam_westboundpro.Aimbot.Settings.LockPart].Position)})
 						Animation:Play()
 					else
-						Camera.CFrame = CFramenew(Camera.CFrame.Position, Environment.Locked.Character[ParentEnvironment.HitPart].Position)
+						Camera.CFrame = CFramenew(Camera.CFrame.Position, getgenv().AirTeam_westboundpro.Aimbot.Locked.Character[getgenv().AirTeam_westboundpro.Settings.HitPart].Position)
 					end
 
 					UserInputService.MouseDeltaSensitivity = 0
 				end
 
-				Environment.FOVCircle.Color = Environment.FOVSettings.LockedColor
+				FOVCircle.Color = getgenv().AirTeam_westboundpro.Aimbot.FOVSettings.LockedColor
 			end
 		end
 	end)
@@ -137,8 +113,8 @@ local function Load()
 	ServiceConnections.InputBeganConnection = UserInputService.InputBegan:Connect(function(Input)
 		if not Typing then
 			pcall(function()
-				if Input.KeyCode == Enum.KeyCode[Environment.Settings.TriggerKey] then
-					if Environment.Settings.Toggle then
+				if Input.KeyCode == Enum.KeyCode[getgenv().AirTeam_westboundpro.Aimbot.Settings.TriggerKey] then
+					if getgenv().AirTeam_westboundpro.Aimbot.Settings.Toggle then
 						Running = not Running
 
 						if not Running then
@@ -151,8 +127,8 @@ local function Load()
 			end)
 
 			pcall(function()
-				if Input.UserInputType == Enum.UserInputType[Environment.Settings.TriggerKey] then
-					if Environment.Settings.Toggle then
+				if Input.UserInputType == Enum.UserInputType[getgenv().AirTeam_westboundpro.Aimbot.Settings.TriggerKey] then
+					if getgenv().AirTeam_westboundpro.Aimbot.Settings.Toggle then
 						Running = not Running
 
 						if not Running then
@@ -168,15 +144,15 @@ local function Load()
 
 	ServiceConnections.InputEndedConnection = UserInputService.InputEnded:Connect(function(Input)
 		if not Typing then
-			if not Environment.Settings.Toggle then
+			if not getgenv().AirTeam_westboundpro.Aimbot.Settings.Toggle then
 				pcall(function()
-					if Input.KeyCode == Enum.KeyCode[Environment.Settings.TriggerKey] then
+					if Input.KeyCode == Enum.KeyCode[getgenv().AirTeam_westboundpro.Aimbot.Settings.TriggerKey] then
 						Running = false; CancelLock()
 					end
 				end)
 
 				pcall(function()
-					if Input.UserInputType == Enum.UserInputType[Environment.Settings.TriggerKey] then
+					if Input.UserInputType == Enum.UserInputType[getgenv().AirTeam_westboundpro.Aimbot.Settings.TriggerKey] then
 						Running = false; CancelLock()
 					end
 				end)
